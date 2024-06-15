973c7dc82b20135ad7f6e279b67c23c03698f938
sean537 <wushaoquan666@outlook.com>
2024-06-15 16:45:21 +0800

    add some main files

---
+++ 537main.cpp
@@ -0,0 +1,19 @@
+/*
+537 USB Disk Manager
+Version 1.0 Beta
+
+537main.cpp
+Copyright(C)537 Studio.2024.All rights reserved.
+*/
+//https://xege.org/manual/api/index.htm
+#include "include/main_window.h"
+int main(){
+	initgraph(640,480,0);
+	
+    setcaption("537 USB Disk Manager");
+    //setrendermode(RENDER_MANUAL);
+	getch();
+
+    closegraph();
+    return 0;
+}
---
+++ include/main_window.h
@@ -0,0 +1,6 @@
+#include <graphics.h> 
+struct{
+	HWND hwnd;//=getHWnd();
+	HINSTANCE hinstance;//=getHInstance();
+	HWND hdc;//=getHDC();
+}mainwindow;
