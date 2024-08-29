1207b8c51eefffeef6e9db62b40d099a79b29ee7
Sean537 <wushaoquan666@outlook.com>
2024-08-29 14:23:01 +0800

    some functions move to head files

--- 537main.cpp
+++ 537main.cpp
@@ -9,10 +9,7 @@ Copyright(C)537 Studio.2024.All rights reserved.
 
 //EGE库API文档：https://xege.org/manual/api/index.htm
 
-#include <iostream>
-#include <string> 
-#include <sstream>  
-#include <windows.h>  
+
 #include "include/graphics.h" 
 #include "537UDM.h"
 #include "include/main_window.h"
@@ -28,10 +25,8 @@ Copyright(C)537 Studio.2024.All rights reserved.
 	#define getch
 #endif
 
-using namespace std;
 
-int GetDeviceInfo();
-void ConvertSizeToString(ULONGLONG size, char* buffer, size_t bufferSize);
+
 
 int main(){
 	ShowConsoleWindow;
@@ -105,65 +100,4 @@ int main(){
 
     closegraph();//关闭绘图窗口 
     return 0;
-}
-
-// 辅助函数，用于将ULLONG转换为更易读的格式  
-void ConvertSizeToString(ULONGLONG size, char* buffer, size_t bufferSize){  
-    if(size<1024){
-        snprintf(buffer,bufferSize,"%llu B",size);  
-    }else if(size<(1024*1024)){  
-        snprintf(buffer,bufferSize,"%.2f KB",static_cast<double>(size)/1024.0);  
-    }else if(size<(1024*1024*1024)){  
-        snprintf(buffer,bufferSize,"%.2f MB",static_cast<double>(size)/(1024.0*1024.0));  
-    }else{  
-        snprintf(buffer,bufferSize,"%.2f GB",static_cast<double>(size)/(1024.0*1024.0*1024.0));  
-    }  
-    buffer[bufferSize-1]='\0'; // 确保字符串以null结尾  
-}  
-  
-int GetDeviceInfo() {  
-    char modulePath[MAX_PATH]={0};  
-    char driveRoot[3]={0};  
-  
-    // 获取当前可执行文件的完整路径  
-    if(GetModuleFileNameA(NULL,modulePath,MAX_PATH)==0){ 
-        cout<<"GetModuleFileNameA failed!"<<endl;  
-        return 1;
-    } 
-  	
-    // 从完整路径中提取盘符  
-    driveRoot[0]=toupper(modulePath[0]);  
-    driveRoot[1]=':';
-    driveRoot[2]='/';
-    driveRoot[3]='\0';  
-  
-    // 将盘符复制到全局变量中  
-    strncpy(DriveLetter, driveRoot, 4);  
-    DriveLetter[2] = '\0'; // 确保DriveLetter以null结尾  
-  
-    // 获取卷信息  
-    if (!GetVolumeInformationA(driveRoot,VolumeName,MAX_PATH,NULL,NULL,NULL,FileSystemName,MAX_PATH)) {  
-        cout<<"GetVolumeInformationA failed!"<<endl;  
-        return 2;  
-    }  
-  
-    // 获取磁盘空间信息  
-    ULARGE_INTEGER totalSpace, freeSpace, totalFreeSpace;  
-    if (!GetDiskFreeSpaceExA(driveRoot, &freeSpace, &totalSpace, &totalFreeSpace)) {  
-        cout<<"GetDiskFreeSpaceExA failed!"<<endl;  
-        return 3;  
-    }  
-  
-    // 转换空间大小为更易读的格式  
-    ConvertSizeToString(totalSpace.QuadPart, TotalSpaceStr, sizeof(TotalSpaceStr));  
-    ConvertSizeToString(freeSpace.QuadPart, FreeSpaceStr, sizeof(FreeSpaceStr));  
-  
-    // 输出结果  
-    cout<<"DriveLetter: "<<DriveLetter<<endl;  
-    cout<<"VolumeName: "<<VolumeName<<endl;  
-    cout<<"FileSystemName: "<<FileSystemName<<endl;  
-    cout<<"TotalSpace: "<<TotalSpaceStr<<endl;  
-    cout<<"FreeSpace: "<<FreeSpaceStr<<endl<<endl;  
-    
-	return 0;
 }
